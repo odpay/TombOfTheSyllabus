f44be1309cafe993fdbe525691ef85344087b5d4
SHays24 <81133539+SHays24@users.noreply.github.com>
2024-05-14 10:27:54 +1000

    Update genlevel.py

--- genlevel.py
+++ genlevel.py
@@ -5,14 +5,15 @@ LVL = "1"
 
 im = Image.open(f'levelsprites/{LVL}.png', 'r')
 
+class colors:
+    RED = (255,0,0)
+    WHITE = (255,255,255)
+    BLUE = (0,0,255)
+    YELLOW = (255,255,0)
+    GREEN = (0,255,0)
+    BLACK = (0,0,0)
+    PURPLE = (93, 63, 211)
 
-RED = (255,0,0)
-WHITE = (255,255,255)
-BLUE = (0,0,255)
-YELLOW = (255,255,0)
-GREEN = (0,255,0)
-BLACK = (0,0,0)
-PURPLE = (93, 63, 211)
 levelData = {}
 level = []
 
@@ -22,18 +23,18 @@ for rownum in range(64):
         col = im.getpixel((pixelnum, rownum))
         col = col[0:3]
         match col:
-            case WHITE:
+            case colors.WHITE:
                 row.append(0)
-            case BLACK:
+            case colors.BLACK:
                 row.append(2)
-            case RED:
+            case colors.RED:
                 row.append(3)
-            case BLUE:
+            case colors.BLUE:
                 levelData["playerSpawn"] = (pixelnum, rownum)
                 row.append(0)
-            case PURPLE:
+            case colors.PURPLE:
                 row.append(4)
-            case GREEN:
+            case colors.GREEN:
                 row.append(5)
             case _:
                 row.append(0)
